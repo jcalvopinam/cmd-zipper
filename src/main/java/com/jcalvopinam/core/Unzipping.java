337e32908e254b367626290452e8f35f918ccf2c
Juan Calvopina <juan.calvopina+dev@gmail.com>
2017-04-21 23:50:16 -0300

    Added GPL License

--- src/main/java/com/jcalvopinam/core/Unzipping.java
+++ src/main/java/com/jcalvopinam/core/Unzipping.java
@@ -1,7 +1,22 @@
+/*
+This file is part of cmd-zipper.
+
+cmd-zipper is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License.
+
+cmd-zipper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+http://www.gnu.org/licenses/
+*/
+
 package com.jcalvopinam.core;
 
-import com.jcalvopinam.utils.Extensions;
 import com.jcalvopinam.domain.CustomFile;
+import com.jcalvopinam.utils.Extensions;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.*;
--- src/main/java/com/jcalvopinam/utils/Extensions.java
+++ src/main/java/com/jcalvopinam/utils/Extensions.java
@@ -1,7 +1,22 @@
+/*
+This file is part of cmd-zipper.
+
+cmd-zipper is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License.
+
+cmd-zipper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+http://www.gnu.org/licenses/
+*/
+
 package com.jcalvopinam.utils;
 
 /**
- * Created by juanca on 4/20/17.
+ * Created by juanca on  4/20/17.
  */
 public enum Extensions {
 
